dedd8a869111395ab8e7387cc113c3dbe7341ed4
Dmitry Rymar <goehoture5454@gmail.com>
2022-09-19 13:36:40 +0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int ticket = 10000;
+        int mile = ticket / 20;
+        int bonus = mile;
+        System.out.println(bonus + " Начисленных миль");
+    }
+}
